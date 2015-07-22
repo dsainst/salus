7b18191f6ca0b86dd97841b9bc06b20440cb968c
dsainst <front_06@inbox.ru>
2015-07-22 18:26:46 +0300

    21 pages

---
+++ php/company-content.php
@@ -0,0 +1,154 @@
+            <div class="g8">
+                    <div class="w60">
+                    <h1 class="title-c">Добавление новой компании</h1>
+                        <form action="#" id="mysort">
+                        <fieldset class="form-group2">
+                        <p>Название компании</p>
+                        <textarea name="company" id="" cols="30" rows="10"></textarea>
+                        </fieldset>
+
+                        <span>Сфера деятельности (можно выбрать несколько)</span>
+
+                        <fieldset class="form-group2">
+
+                        <legend>Морские грузоперевозки</legend>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us1" value="us" />
+                                        <label for="us1">Транспортно-экспедиторские</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us2" value="gruz" />
+                                        <label for="us2">Логистические</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us3" value="disp" />
+                                        <label for="us3">Перевозчики, судовладельцы</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us4" value="strah" />
+                                        <label for="us4">Таможенные брокеры</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us5" value="disp2" />
+                                        <label for="us5">Владельцы контейнерного парка</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us6" value="disp3" />
+                                        <label for="us6">Операторы портовой инфраструктуры</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us7" value="disp4" />
+                                        <label for="us7">Терминальные комплексы</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us8" value="disp5" />
+                                        <label for="us8">Страховые компании</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us9" value="disp6" />
+                                        <label for="us9">Фрахтовые брокеры</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us10" value="disp7" />
+                                        <label for="us10">Морские агенты</label>
+                                    </div>
+                                    <br>
+                                </fieldset>
+
+                                <fieldset class="form-group2">
+                                <legend>Автомобильные грузоперевозки</legend>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us1" value="us" />
+                                        <label for="us1">Транспортно-экспедиторские</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us2" value="gruz" />
+                                        <label for="us2">Логистические</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us3" value="disp" />
+                                        <label for="us3">Перевозчики</label>
+                                    </div>
+                                    <br>
+                                </fieldset>
+                                <div class="form-group2">
+                            <input type="button" class="btn-shadow save" value="Сохранить изменения">
+                            <input type="button" class="btn-shadow cancel" value="Отмена">
+                            <div class="clearfix"></div>
+                        </div>
+                        </form>
+                        <div class="paginate">
+                            <ul>
+                                <li class="prev">
+                                    <a href="#"></a>
+                                </li>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#" class="current">4</a></li>
+                                <li><a href="#">5</a></li>
+                                <li><a href="#">6</a></li>
+                                <li class="next">
+                                    <a href="#"></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="w40">
+                        <div class="wrap">
+                            <form action="#" id="myopt">
+                                <div class="form-group2">
+                                    <input type="button" value="Добавить новую компанию" class="btn-submit">
+                                </div>
+                                <fieldset class="form-group2">
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us1" value="us" />
+                                        <label for="us1">Транспортно-экспедиторские</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us2" value="gruz" />
+                                        <label for="us2">Логистические</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us3" value="disp" />
+                                        <label for="us3">Перевозчики, судовладельцы</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us4" value="strah" />
+                                        <label for="us4">Таможенные брокеры</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us5" value="disp2" />
+                                        <label for="us5">Владельцы контейнерного парка</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us6" value="disp3" />
+                                        <label for="us6">Операторы портовой инфраструктуры</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us7" value="disp4" />
+                                        <label for="us7">Терминальные комплексы</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us8" value="disp5" />
+                                        <label for="us8">Страховые компании</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us9" value="disp6" />
+                                        <label for="us9">Фрахтовые брокеры</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us10" value="disp7" />
+                                        <label for="us10">Морские агенты</label>
+                                    </div>
+                                    <div class="radio-group">
+                                        <input type="checkbox" name="uslugi" id="us11" value="disp8" />
+                                        <label for="us11" class="active">Все компании   </label>
+                                    </div>
+                                    <br>
+                                </fieldset>
+                                
+                            </form>
+                        </div>
+                    </div>
+            </div>
